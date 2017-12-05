041bcec1ba062c4b36434faa34fb64a80e57aa49
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-05 13:08:29 +0000

    adds hTML conversion function

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,15 +1,22 @@
-function testReturnsHTML () {
-  var noteListView = new NoteListView (DoubleNoteListModel)
-  assert.isTrue(noteListView.returnsHTML() === '<ul><li><div></div></li></ul>')
-}
-testReturnsHTML ()
+// function testReturnsHTML () {
+//   var noteListView = new NoteListView (DoubleNoteListModel)
+//   assert.isTrue(noteListView.returnsHTML() === '<ul><li><div></div></li></ul>')
+// }
+// testReturnsHTML ()
 
 function testReturnsHTMLNote () {
-  var noteListView = new NoteListView (DoubleNoteListModel('Favourite food: pesto'))
+  var noteListView = new NoteListView (DoubleNoteListModel(['Favourite food: pesto']))
   assert.isTrue(noteListView.returnsHTML() === '<ul><li><div>Favourite food: pesto</div></li></ul>')
-
 }
 testReturnsHTMLNote ()
+
+function testReturnsHTMLMultipleNotes () {
+  var noteListView = new NoteListView (DoubleNoteListModel(['Favourite food: pesto', 'Favourite drink: seltzer']))
+  console.log(noteListView.returnsHTML())
+  console.log(noteListView.noteListModelClass.join('</div></li><li><div>'))
+  assert.isTrue(noteListView.returnsHTML() === '<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>')
+}
+testReturnsHTMLMultipleNotes ()
 
 
 // Doubles:
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,11 +1,15 @@
 (function (exports) {
 
   function NoteListView (noteListModel = NoteListModel) {
-    this.noteListModelClass = noteListModel
+    this.noteListModelClass = noteListModel  // *notes(text)
   }
 
-  NoteListView.prototype.returnsHTML = function (text) {
-    return '<ul><li><div>'+this.noteListModelClass(text)+'</div></li></ul>'
+  NoteListView.prototype.returnAllList = function () {
+    return this.noteListModelClass
+  }
+
+  NoteListView.prototype.returnsHTML = function () {
+    return '<ul><li><div>'+ this.noteListModelClass.join('</div></li><li><div>') + '</div></li></ul>'
   }
 
   exports.NoteListView = NoteListView
